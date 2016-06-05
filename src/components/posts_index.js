9a423cbc4194ddb5a15c2c5b0dda312bba7ec0c2
Jared <jaredcmurphy@gmail.com>
2016-06-05 14:32:25 -0400

    adds react router link in button for postsnew route

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchPosts } from '../actions/index';
+import { Link } from 'react-router';
 
 class PostsIndex extends Component {
   componentWillMount() {
@@ -10,8 +11,14 @@ class PostsIndex extends Component {
 
   render() {
     return (
-    <div>List of blog posts</div>
-    <button>New Post</button>
+    <div>
+      <div className="text-xs-right">
+        <Link to="/posts/new" className="btn btn-primary">
+          Add a Post
+        </Link>
+      </div>
+      List of blog posts
+    </div>
     );
   }
 }
