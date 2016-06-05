7330348d407f3d7bc3d04a9514070dc974b50c62
Jared <jaredcmurphy@gmail.com>
2016-06-05 18:39:28 -0400

    data successfully rendered on post show

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -3,12 +3,29 @@ import { connect } from 'react-redux';
 import { fetchPost } from '../actions/index';
 
 class PostsShow extends Component {
-  render() {
-    return (
-      <div>Show post {this.props.params.id}</div>
+  componentWillMount() {
+    this.props.fetchPost(this.props.params.id);
+  }
 
-    )
+  render() {
+    const { post } = this.props;
+
+    if (!post){
+      return <div>Loading...</div>;
+    }
+
+    return (
+      <div>
+        <h3>{post.title}</h3>
+        <h6>Categories: {post.categories}</h6>
+        <p>{post.content}</p>
+      </div>
+    );
   }
 }
 
-export default connect(null, { fetchPost })(PostsShow);
+function mapStateToProps(state){
+  return { post: state.posts.post };
+}
+
+export default connect(mapStateToProps, { fetchPost })(PostsShow);
