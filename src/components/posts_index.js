874567d5d6f3d73faaa08921565b9e2f7d425552
Jared <jaredcmurphy@gmail.com>
2016-06-05 14:18:44 -0400

    action creator returns 200 response with data

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchPosts } from '../actions/index';
 
 class PostsIndex extends Component {
   componentWillMount() {
+    this.props.fetchPosts();
     console.log('this is the time to fetch data');
   }
 
@@ -12,4 +16,7 @@ class PostsIndex extends Component {
   }
 }
 
-export default PostsIndex;
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ fetchPosts }, dispatch);
+}
+export default connect(null, mapDispatchToProps)(PostsIndex);
